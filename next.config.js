ad13141169424b7a95d3b508391b06635840ad57
Juan Pablo Solano <hello@wondercraft.co>
2022-10-28 11:29:48 -0500

    image

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+      'images.ctfassets.net',
+      'picsum.photos',
+      'undefined', //because some images could be not set in contentful and would unnecessarily break the build
+    ],
+  },
 }
 
 module.exports = nextConfig
